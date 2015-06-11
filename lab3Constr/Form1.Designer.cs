bc0543f150fd341e2e8558bde36b9a0d60eaf0a4
Karoliner <starbursts@ya.ru>
2015-06-12 01:43:55 +0300

    V2
    Interface upgrade

--- lab3Constr/Form1.Designer.cs
+++ lab3Constr/Form1.Designer.cs
@@ -40,7 +40,7 @@
             // metroLabel1
             // 
             this.metroLabel1.AutoSize = true;
-            this.metroLabel1.Location = new System.Drawing.Point(23, 70);
+            this.metroLabel1.Location = new System.Drawing.Point(23, 126);
             this.metroLabel1.Name = "metroLabel1";
             this.metroLabel1.Size = new System.Drawing.Size(123, 19);
             this.metroLabel1.TabIndex = 1;
@@ -48,9 +48,9 @@
             // 
             // metroButton1
             // 
-            this.metroButton1.Location = new System.Drawing.Point(531, 89);
+            this.metroButton1.Location = new System.Drawing.Point(435, 63);
             this.metroButton1.Name = "metroButton1";
-            this.metroButton1.Size = new System.Drawing.Size(157, 23);
+            this.metroButton1.Size = new System.Drawing.Size(200, 60);
             this.metroButton1.TabIndex = 2;
             this.metroButton1.Text = "Задать новые значения";
             this.metroButton1.UseSelectable = true;
@@ -58,19 +58,19 @@
             // 
             // sort1
             // 
-            this.sort1.Location = new System.Drawing.Point(65, 130);
+            this.sort1.Location = new System.Drawing.Point(23, 63);
             this.sort1.Name = "sort1";
-            this.sort1.Size = new System.Drawing.Size(204, 62);
+            this.sort1.Size = new System.Drawing.Size(200, 60);
             this.sort1.TabIndex = 3;
-            this.sort1.Text = "Сортировка ставками";
+            this.sort1.Text = "Сортировка вставками";
             this.sort1.UseSelectable = true;
             this.sort1.Click += new System.EventHandler(this.sort1_Click);
             // 
             // fastSort
             // 
-            this.fastSort.Location = new System.Drawing.Point(385, 130);
+            this.fastSort.Location = new System.Drawing.Point(229, 63);
             this.fastSort.Name = "fastSort";
-            this.fastSort.Size = new System.Drawing.Size(187, 62);
+            this.fastSort.Size = new System.Drawing.Size(200, 60);
             this.fastSort.TabIndex = 4;
             this.fastSort.Text = "Быстрая сортировка";
             this.fastSort.UseSelectable = true;
@@ -78,35 +78,35 @@
             // metroLabel2
             // 
             this.metroLabel2.AutoSize = true;
-            this.metroLabel2.Location = new System.Drawing.Point(23, 206);
+            this.metroLabel2.Location = new System.Drawing.Point(23, 174);
             this.metroLabel2.Name = "metroLabel2";
-            this.metroLabel2.Size = new System.Drawing.Size(69, 19);
+            this.metroLabel2.Size = new System.Drawing.Size(67, 19);
             this.metroLabel2.TabIndex = 5;
             this.metroLabel2.Text = "Результат:";
             // 
             // InitialData
             // 
             this.InitialData.Lines = new string[0];
-            this.InitialData.Location = new System.Drawing.Point(23, 89);
+            this.InitialData.Location = new System.Drawing.Point(23, 148);
             this.InitialData.MaxLength = 32767;
             this.InitialData.Name = "InitialData";
             this.InitialData.PasswordChar = '\0';
             this.InitialData.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.InitialData.SelectedText = "";
-            this.InitialData.Size = new System.Drawing.Size(502, 23);
+            this.InitialData.Size = new System.Drawing.Size(612, 23);
             this.InitialData.TabIndex = 7;
             this.InitialData.UseSelectable = true;
             // 
             // resultData
             // 
             this.resultData.Lines = new string[0];
-            this.resultData.Location = new System.Drawing.Point(23, 228);
+            this.resultData.Location = new System.Drawing.Point(23, 196);
             this.resultData.MaxLength = 32767;
             this.resultData.Name = "resultData";
             this.resultData.PasswordChar = '\0';
             this.resultData.ScrollBars = System.Windows.Forms.ScrollBars.None;
             this.resultData.SelectedText = "";
-            this.resultData.Size = new System.Drawing.Size(665, 23);
+            this.resultData.Size = new System.Drawing.Size(612, 23);
             this.resultData.TabIndex = 8;
             this.resultData.UseSelectable = true;
             // 
@@ -114,7 +114,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 281);
+            this.ClientSize = new System.Drawing.Size(658, 238);
             this.Controls.Add(this.resultData);
             this.Controls.Add(this.InitialData);
             this.Controls.Add(this.metroLabel2);
